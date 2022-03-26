750183c5475eb9102d04555f10bbd4f95ace2b0e
spth <spth@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2022-03-26 19:51:03 +0000

    More regression test infrastructure, picked from patch #430 by Felix Salfelder (felixs). And fix test file extension for parametrized test.

--- sdcc/support/regression/ports/mcs51-common/spec.mk
+++ sdcc/support/regression/ports/mcs51-common/spec.mk
@@ -64,12 +64,4 @@ $(PORT_CASES_DIR)/fwk.lib: $(srcdir)/fwk/lib/fwk.lib $(PORTS_DIR)/mcs51-common/f
 	cat < $(srcdir)/fwk/lib/fwk.lib > $@
 	cat < $(PORTS_DIR)/mcs51-common/fwk.lib >> $@
 
-# run simulator with SIM_TIMEOUT seconds timeout
-%.out: %$(BINEXT) $(CASES_DIR)/timeout
-	mkdir -p $(dir $@)
-	-$(CASES_DIR)/timeout $(SIM_TIMEOUT) $(EMU) $(EMU_PORT_FLAG) $(EMU_FLAGS) $< < $(PORTS_DIR)/$(PORT_BASE)/uCsim.cmd > $@ \
-	  || echo -e --- FAIL: \"timeout, simulation killed\" in $(<:$(BINEXT)=.c)"\n"--- Summary: 1/1/1: timeout >> $@
-	$(PYTHON) $(srcdir)/get_ticks.py < $@ >> $@
-	-grep -n FAIL $@ /dev/null || true
-
 _clean:
